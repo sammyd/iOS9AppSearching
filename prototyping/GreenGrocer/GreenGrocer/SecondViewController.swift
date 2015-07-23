de656863ddd439bb606883e8427c0b4e51d588e1
Sam Davies <sam@razeware.com>
2015-07-23 12:27:34 +0100

    Initial commit of the prototyping project

---
+++ prototyping/GreenGrocer/GreenGrocer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GreenGrocer
+//
+//  Created by Sam Davies on 23/07/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
