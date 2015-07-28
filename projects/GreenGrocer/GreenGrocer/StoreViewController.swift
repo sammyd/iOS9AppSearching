c9d55d49e931ce9d5ad99163c607bc0d2aed0642
Sam Davies <sam@razeware.com>
2015-07-28 16:36:41 +0100

    Video 1: Challenge completed
    This adds the "contact" page to the index.

--- projects/GreenGrocer/GreenGrocer/StoreViewController.swift
+++ projects/GreenGrocer/GreenGrocer/StoreViewController.swift
@@ -23,12 +23,16 @@
 
 import UIKit
 import MapKit
+import MobileCoreServices
+import CoreSpotlight
+
+let storeDetailsActivityID = "com.razeware.GreenGrocer.storeDetails"
 
 class StoreViewController: UIViewController {
   
   @IBOutlet weak var mapView: MKMapView!
   let coordinate = CLLocationCoordinate2D(latitude: 35.816667, longitude: -80.258611)
-  let phoneNumber = "(202) 555 0198"
+  let phoneNumber = "2025550198"
   let streetAddress = "2627 Mulberry Street\nLexington, NC 27292"
   let storeName = "Ray's Fruit Emporium"
   
@@ -38,6 +42,32 @@ class StoreViewController: UIViewController {
     // Do any additional setup after loading the view.
     let mapRect = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
     mapView.setRegion(mapRect, animated: false)
+    
+    userActivity = prepareUserActivity()
   }
   
+}
+
+
+extension StoreViewController {
+  override func updateUserActivityState(activity: NSUserActivity) {
+    activity.title = storeName
+    activity.keywords = Set(["shopping", "fruit", "lists", "greengrocer", storeName])
+    let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeContact as String)
+    attributeSet.contentDescription = "The best greengrocer in town!"
+    attributeSet.phoneNumbers = [phoneNumber]
+    attributeSet.longitude = coordinate.longitude
+    attributeSet.latitude = coordinate.latitude
+    attributeSet.supportsPhoneCall = true
+    
+    activity.contentAttributeSet = attributeSet
+  }
+  
+  private func prepareUserActivity() -> NSUserActivity {
+    let activity = NSUserActivity(activityType: storeDetailsActivityID)
+    activity.eligibleForHandoff = true
+    activity.eligibleForPublicIndexing = true
+    activity.eligibleForSearch = true
+    return activity
+  }
 }
